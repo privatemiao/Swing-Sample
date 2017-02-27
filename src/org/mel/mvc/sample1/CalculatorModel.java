92244d37b408b679abc76a11d3c8f3c54c724f24
Mel Liu <privatemiao@gmail.com>
2017-02-28 00:23:31 +0800

    first

---
+++ src/org/mel/mvc/sample1/CalculatorModel.java
@@ -0,0 +1,14 @@
+package org.mel.mvc.sample1;
+
+public class CalculatorModel {
+	private int calculationValue;
+
+	public void addTwoNumbers(int firstNumber, int secondNumber) {
+		calculationValue = firstNumber + secondNumber;
+	}
+
+	public int getCalculationValue() {
+		return calculationValue;
+	}
+
+}
